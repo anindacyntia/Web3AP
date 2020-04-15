1688ef47c733207e6644b96829d5cbc36fcd76b3
Aninda Cyntia <anindacyntia@users.noreply.github.com>
2020-04-15 16:56:43 +0700

    Updates

---
+++ precache-manifest.2278e9974116c37db4653123284abd86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77ba7fb11256576f643da06a4bcfb95",
+    "url": "/Web3AP-anindacyntia/index.html"
+  },
+  {
+    "revision": "214d83072bf55a1eed6d",
+    "url": "/Web3AP-anindacyntia/static/css/main.42639e65.chunk.css"
+  },
+  {
+    "revision": "1e3add2ed4b35ef66184",
+    "url": "/Web3AP-anindacyntia/static/js/2.30169821.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/Web3AP-anindacyntia/static/js/2.30169821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214d83072bf55a1eed6d",
+    "url": "/Web3AP-anindacyntia/static/js/main.117d3e54.chunk.js"
+  },
+  {
+    "revision": "a565d41db3eb3084bf28",
+    "url": "/Web3AP-anindacyntia/static/js/runtime-main.9cb82d2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web3AP-anindacyntia/static/media/logo.5d5d9eef.svg"
+  }
+]);
